3daed357164d38f4a15d7d5571eb64f4721880fb
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-03-29 17:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function calculateMinCost() {
   var inputData = document.querySelector("#rope-lengths").vlaue;
 	var inputArr = inputData.split(",");
 	// alert(typeof inputArr[0]);
-	for (let i = 0; i < inputArr.length; i++) {
+	for (var i = 0; i < inputArr.length; i++) {
 		inputArr[i] = Number(inputArr[i]);
 	}
 
 	var cost = 0;
 	inputArr.sort(function (a,b) {return a-b;});
-   while (inputArr.length>1) {
+  
+	while (inputArr.length>1) {
    	 var newRope = inputArr[0] + inputArr[1];
 	   cost += newRope;
 
